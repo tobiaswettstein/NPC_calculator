77b3cc34953b8c6b4b6d6a2692e6d4f8e2fdedb4
tobiaswettstein <wettstein.usa@gmail.com>
2019-08-02 10:08:20 -0700

    new fix

--- npc_calculator/npc_calculator/settings.py
+++ npc_calculator/npc_calculator/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '5nd)p6iz+c823p34$3%29e0(ggp5i&1i6ldokgqsj+c)$zdv&s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ["serene-bayou-42989.herokuapp.com"]
+ALLOWED_HOSTS = ["serene-bayou-42989.herokuapp.com"]
 
 
 
